412457786c25f08cf4d81b87542492ada09367bf
acparkgh <acpark123@gmail.com>
2021-05-04 00:46:18 -0400

    using default arguments

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const booking = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 99,
+  price = 999,
+) {
+  const bookingObj = {
+    flightNum,
+    numPassengers,
+    price,
+  }
+  console.log(bookingObj);
+  booking.push(bookingObj);
+}
+
+createBooking("LH123");
+createBooking("AB373", 2, 199);
+createBooking("BE372", undefined, 200);
+console.log(booking);
