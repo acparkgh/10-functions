d75d68310fe8b1f9ec7386e4a8da21e72c944831
acparkgh <acpark123@gmail.com>
2021-05-05 00:37:28 -0400

    call and apply methods

--- starter/script.js
+++ starter/script.js
@@ -38,7 +38,7 @@ const checkIn = function (flightNum, passenger) {
   } else {
     alert("Wrong passport!");
   };
-}
+};
 
 // checkIn(flight, jonas);
 // console.log(flight);
@@ -91,7 +91,93 @@ const high5 = function () {
 // document.body.addEventListener("click", high5);
 document.querySelector("h1").addEventListener("click", high5);
 
+// const greet = function (greeting) {
+//   console.log(greeting);
+//   return (
+//     function (name) {
+//       console.log(`${greeting} ${name}`);
+//     }
+//   )
+// }
 
+// const greet = (greeting) => { return (name) => {console.log(`${greeting} ${name}`)} }
+const greet = greeting => name => console.log(`${greeting} ${name}`);
 
+const greeter = greet("Hi");
 
+greeter("Andrew");
+greeter("Jonas");
+greet("Hello")("Kim");
 
+const lufthansa = {
+  airline: "Lufthansa",
+  iataCode: "LH",
+  bookings: [],
+  // book: function () {}
+  book(flightNum, passengerName) {
+    console.log(
+      `${passengerName} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    // this.bookings.push({ flight: `${this.iataCode}${flightNum}`, passengerName: passengerName })
+    this.bookings.push( {
+      flight: `${this.iataCode}${flightNum}`,
+      passengerName,
+    } );
+  }
+};
+
+lufthansa.book(239, "Jonas Schmedtmann");
+lufthansa.book(635, "John Smith");
+console.log(lufthansa);
+console.log(lufthansa.bookings);
+
+const eurowings = {
+  airline: "Eurowings",
+  iataCode: "EW",
+  bookings: [],
+};
+
+const book = lufthansa.book;
+
+book.call(eurowings, 23, "Sarah Williams");
+console.log(eurowings);
+
+book.call(lufthansa, 239, "Mary Cooper");
+console.log(lufthansa);
+
+const swiss = {
+  airline: "Swiss Air Lines",
+  iataCode: "LX",
+  bookings: [],
+}
+
+book.call(swiss, 583, "Mary Cooper");
+console.log(swiss);
+
+const flightData = [583, "George Cooper"];
+book.apply(swiss, flightData);
+console.log(swiss)
+
+book.apply(eurowings, [856, "Mike Smith"]);
+console.log(eurowings);
+
+const flightInfo = [123, "Andrew Park"];
+book.call(eurowings, ...flightInfo);
+console.log(eurowings);
+
+const reserveSeat = function (flightNum, passengerName) {
+  console.log(
+    `${passengerName} reserved a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+  );
+  // this.bookings.push({ flight: `${this.iataCode}${flightNum}`, passengerName: passengerName })
+  this.bookings.push( {
+    flight: `${this.iataCode}${flightNum}`,
+    passengerName,
+  } );
+}
+
+reserveSeat.call(eurowings, 987, "John Smith");
+console.log(eurowings);
+
+reserveSeat.apply(lufthansa, [555, "Lucky Start"]);
+console.log(lufthansa);
